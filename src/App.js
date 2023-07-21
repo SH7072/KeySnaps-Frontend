ab9b6617d2b2b6c335aa93f392c8bf34649ba127
Shubham Chandwani <chandwanishubham@gmail.com>
2023-07-21 14:40:09 +0530

    navigation

--- src/App.js
+++ src/App.js
@@ -31,12 +31,15 @@ function App() {
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path="/" element={<Root />}>
-        <Route path="signup" element={<Signup />} />
-        <Route path="register" element={<Login />} />
+        <Route path="register" element={<Signup />} />
+        <Route path="login" element={<Login />} />
         <Route path="home" element={<Home />} />
         <Route path="userInfo" element={<UserInfo />} />
         <Route path="practice" element={<Practice />} />
         <Route path="leaderboard" element={<LeaderBoard />} />
+        <Route path="lobby" element={<Lobby />} />
+        <Route path="createlobby" element={<CreateLobby />} />
+        <Route path="joinlobby" element={<JoinLobby />} />
         <Route path="*" element={<h1>Not Found</h1>} />
       </Route>
     ));
